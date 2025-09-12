69ccb3010407a5eeaadc3368aa477fd1d6dfe65c
Kai Kunze <kaikunze@me.com>
2025-09-13 00:28:40 +0900

    Initial Commit

---
+++ aimemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aimemo
+//
+//  Created by kai on 13.09.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aimemo/aimemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  aimemoApp.swift
+//  aimemo
+//
+//  Created by kai on 13.09.25.
+//
+
+import SwiftUI
+
+@main
+struct aimemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
